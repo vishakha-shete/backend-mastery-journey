41f5e9d115e449ad2621f6129ea262dddf8e2c60
vishakha-shete <vishakhashete2@gmail.com>
2026-01-27 15:48:25 +0530

    completed day-1 of my backend journey learn how to install packages and run the server

---
+++ DAY-1/cat-me/cat-me.js
@@ -0,0 +1,6 @@
+const catMe = require("cat-me")
+console.log(catMe());
+
+//learn how to install packages
+// and how to run javascript file outside the browser
+
---
+++ DAY-1/runserver/Server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+
+const app = express() //created server
+
+app.listen(3000) //run the server
+
+// learn how to run the server
